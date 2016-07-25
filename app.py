e2b9a60808cd52a8e51d96f4c457eb23ec035e80
Essa <essa@EssaMacbook.local>
2016-07-26 02:02:49 +0400

    Connect to mongoDB

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 
 
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient(host='ec2-54-173-3-167.compute-1.amazonaws.com')
+
+mobileDB = client.get_database('mobile')
+
+users = mobileDB.get_collection('users')
+
+
 
 @app.route('/')
 def home():
     return '<h1>Halla</h1>'
 
+@app.route('/<name>')
+def user(name):
+    user = users.find({'name':name})
+
+    return user.next()['mobile']
 
 if __name__ == '__main__':
     app.run(debug=True,port=8181)
